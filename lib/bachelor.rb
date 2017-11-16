64d99b177c6a911b413d43e418b108d2fc278990
Kelly Lai <zhiyinl5633@hstat.org>
2017-11-16 02:39:29 +0000

    finish bachelor lab

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,54 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split.first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season,contestants|
+    contestants.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  count = 0
+  data.each do |season,contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        count +=1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |season,contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  total_age = 0
+  contestant_num = 0
+  data[season].each do |contestant|
+    total_age += contestant["age"].to_f
+    contestant_num += 1
+  end
+  (total_age / contestant_num).round
 end
